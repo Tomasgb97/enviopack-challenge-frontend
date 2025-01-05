bc90a87ad4576329a72ee1ecb4f692115b5e34ce
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-05 19:33:52 -0300

    feat(UserContext): add AddCreditsToUser function and integrate with Checkout component

--- src/routes/Checkout/Checkout.tsx
+++ src/routes/Checkout/Checkout.tsx
@@ -1,16 +1,37 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import Button from '../../components/Common/Button';
 import { getCheckoutMessage } from '../../utils/checkoutMessage';
+import toast, { Toaster } from 'react-hot-toast';
+import { useUserProfile } from '../../contexts/UserContext';
+import { useEffect } from 'react';
 
 const Checkout = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const successStatus = searchParams.get('success');
+  const { AddCreditsToUser } = useUserProfile();
 
   const isSuccess = successStatus === 'true';
 
+  useEffect(() => {
+    if (!isSuccess) {
+      toast(
+        'Notamos que te quedaste sin creditos. Así que te hicimos llegar algunos mas através de Enviopack 👀',
+        { duration: 8000 }
+      );
+      toast('+10.000 creditos ! Vuelve al catalogo para usarlos.', {
+        duration: 8000,
+      });
+
+      setTimeout(() => {
+        AddCreditsToUser(10000);
+      }, 1000);
+    }
+  }, []);
+
   return (
     <div className="w-full flex flex-col justify-center items-center">
+      <Toaster position="bottom-right"></Toaster>
       <h1 className="font-bold text-4xl">Estado de la compra</h1>
 
       <div className="w-full max-w-checkout-max p-4 flex flex-col items-center gap-4 rounded shadow-md shadow-black">
