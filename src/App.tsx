64f286fcc7f0a4f411e9bd30e2aa0d5d9d1ce7d1
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-05 09:38:46 -0300

    build(packages, app tsx): Installs tailwind dependencies and adds it to global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Logo from "/Logo.svg";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="w-full flex flex-col justify-center items-center">
+        <h1>Tailwind envioPack</h1>
+        <img src={Logo} alt="" />
+      </div>
+    </>
+  );
 }
 
 export default App;
