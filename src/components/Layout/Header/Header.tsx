2c5fe65eef4ff1475109a3f8513e6d0886d00081
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-05 17:25:31 -0300

    feat(App, Header): Add Cart route and integrate navigation in Header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,9 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import { useUserProfile } from '../../../contexts/UserContext';
 import { useCartStore } from '../../../stores/cart-store';
 
 const Header = () => {
   const { userProfile } = useUserProfile();
   const { items } = useCartStore();
+  const navigate = useNavigate();
 
   const UserData = () => {
     return (
@@ -11,7 +13,12 @@ const Header = () => {
         {userProfile && (
           <span>{userProfile?.firstName + ' ' + userProfile?.lastName}</span>
         )}
-        <span className="font-bold">Carrito({items.length})</span>
+        <span
+          onClick={() => navigate('/cart')}
+          className="font-bold cursor-pointer"
+        >
+          Carrito({items.length})
+        </span>
         <span>Crédito ${userProfile?.credit}</span>
       </div>
     );
@@ -23,7 +30,12 @@ const Header = () => {
 
   return (
     <header className="flex flex-col gap-3 sm:gap-0 sm:flex-row  justify-between items-center p-2 bg-main-black text-white">
-      <h2 className="text-lg font-bold">Tienda de Productos</h2>
+      <h2
+        onClick={() => navigate('/')}
+        className="text-lg cursor-pointer font-bold"
+      >
+        Tienda de Productos
+      </h2>
       {userProfile == null ? <NoUser /> : <UserData />}
     </header>
   );
---
+++ src/routes/Cart/Cart.tsx
@@ -0,0 +1,9 @@
+const Cart = () => {
+  return (
+    <>
+      <h1 className="font-bold text-4xl">Carrito</h1>
+    </>
+  );
+};
+
+export default Cart;
