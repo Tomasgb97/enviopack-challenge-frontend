cb23b7945dc6414edd075e277b24b0292ff9e5d0
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-05 18:42:39 -0300

    feat(Cart): Enhance cart container with empty state message and navigation button; update checkout flow to clear cart

--- src/components/Cart/Container/Container.tsx
+++ src/components/Cart/Container/Container.tsx
@@ -2,28 +2,45 @@ import React from 'react';
 import { Product } from '../../../types/product';
 import CartItem from '../Cart-item/CartItem';
 import { useCartStore } from '../../../stores/cart-store';
+import Button from '../../Common/Button';
+import { useNavigate } from 'react-router-dom';
 
 interface ContainerProps {
   items: Product[];
 }
 const CartContainer: React.FC<ContainerProps> = ({ items }) => {
   const { removeItem } = useCartStore();
+  const navigate = useNavigate();
   return (
     <div className=" w-full min-h-96 flex flex-col gap-4">
-      {items.map((item) => {
-        return (
-          <CartItem
-            key={item.id}
-            onDelete={() => {
-              removeItem(item.id);
-            }}
-            image={'/images/image-product.jpg'}
-            title={item.title}
-            price={item.price}
-            discount={item.discount}
-          />
-        );
-      })}
+      {items.length == 0 && (
+        <div className="flex-col justify-center items-center">
+          <h1>
+            Tu Carrito esta vacio, regresa al menu catalogo para cargar mas
+            items
+          </h1>
+          <Button
+            variant="Add"
+            onClick={() => navigate('/')}
+            text="Volver al Carrito"
+          ></Button>
+        </div>
+      )}
+      {items.length > 0 &&
+        items.map((item) => {
+          return (
+            <CartItem
+              key={item.id}
+              onDelete={() => {
+                removeItem(item.id);
+              }}
+              image={'/images/image-product.jpg'}
+              title={item.title}
+              price={item.price}
+              discount={item.discount}
+            />
+          );
+        })}
     </div>
   );
 };
