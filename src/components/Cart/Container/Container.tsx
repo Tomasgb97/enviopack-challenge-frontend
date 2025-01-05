ef2e028b06cd056b5a8f64d37c77a8e0af8e34d3
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2025-01-05 18:37:35 -0300

    feat(Cart): Implement item removal functionality in cart container

--- src/components/Cart/Container/Container.tsx
+++ src/components/Cart/Container/Container.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Product } from '../../../types/product';
 import CartItem from '../Cart-item/CartItem';
+import { useCartStore } from '../../../stores/cart-store';
 
 interface ContainerProps {
   items: Product[];
 }
 const CartContainer: React.FC<ContainerProps> = ({ items }) => {
+  const { removeItem } = useCartStore();
   return (
     <div className=" w-full min-h-96 flex flex-col gap-4">
       {items.map((item) => {
         return (
           <CartItem
             key={item.id}
-            onDelete={() => {}}
+            onDelete={() => {
+              removeItem(item.id);
+            }}
             image={'/images/image-product.jpg'}
             title={item.title}
             price={item.price}
